5aed428694f1cdc52767bb6103fdb180f35d5a42
Antoine MULIN <antoine.mulin@gmail.com>
2014-12-10 16:27:28 +0100

    Creation des fichiers, copie et adaptation de la libft

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/12/10 14:06:47 by amulin            #+#    #+#             */
+/*   Updated: 2014/12/10 14:14:04 by amulin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+
+# define BUFF_SIZE 32
+
+int	get_next_line(int const fd, char **line);
+
+#endif
